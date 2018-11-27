579d3d0a55e9f6ea1a1c88eebbb0db13c96fd10b
Gambhiro <gambhiro.bhikkhu.85@gmail.com>
2018-11-27 06:19:54 +0000

    ebook

--- helpers/chapters_to_asciidoc.sh
+++ helpers/chapters_to_asciidoc.sh
@@ -4,6 +4,11 @@ for i in ./manuscript/tex/[0-9][0-9]*.tex; do
     ./helpers/tex_to_asciidoc.sh "$i"
 done
 
-./helpers/tex_to_asciidoc.sh ./manuscript/tex/foreword.tex
+./helpers/tex_to_asciidoc.sh ./manuscript/tex/nota-do-tradutor.tex
 
 ./helpers/tex_to_asciidoc.sh ./manuscript/tex/preface.tex
+
+./helpers/tex_to_asciidoc.sh ./manuscript/tex/introduction.tex
+
+./helpers/tex_to_asciidoc.sh ./manuscript/tex/sobre-o-autor.tex
+
